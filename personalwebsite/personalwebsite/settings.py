a3bfb466ece27a634543c26bdf9ed3aa4aba371d
Walter Nam <49884140+wnam98@users.noreply.github.com>
2021-03-25 15:31:39 +0900

    basic components completed, color scheme complete, profile picture rendered

--- personalwebsite/personalwebsite/settings.py
+++ personalwebsite/personalwebsite/settings.py
@@ -118,4 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/images/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
